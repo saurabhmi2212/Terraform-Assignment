f866efeb5c613609a563cb057d35d734ead3461e
Saurabh Mishra <65559896+saurabhmi2212@users.noreply.github.com>
2025-08-15 11:00:19 +0530

    Create variables.tf

---
+++ modules/log_analytics/variables.tf
@@ -0,0 +1,14 @@
+variable "workspace_name" {
+  description = "The name of the Log Analytics Workspace"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
