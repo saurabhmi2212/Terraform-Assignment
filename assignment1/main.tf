0e80a06483a5e0a1c41818297dea0a6742f8fde7
Saurabh Mishra <65559896+saurabhmi2212@users.noreply.github.com>
2025-08-15 10:19:06 +0530

    Update main.tf

--- assignment1/main.tf
+++ assignment1/main.tf
@@ -1,58 +1,55 @@
-data "azurerm_public_ip" "pip" {
-  name                = "public_ip"
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_public_ip" "pip" {
-  name                = "${var.vm_name}-public-ip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Basic"
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = var.nsg_id
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = var.vm_name
+# --- Cloud-init script for NGINX installation
+resource "local_file" "cloud_init" {
+  filename        = "cloud-init.yaml"
+  content         = <<-EOT
+  #cloud-config
+  package_upgrade: true
+  packages:
+    - nginx
+  runcmd:
+    - systemctl start nginx
+    - systemctl enable nginx
+  EOT
+}
+
+# --- Module calls
+module "network" {
+  source                = "./modules/network"
   resource_group_name   = var.resource_group_name
   location              = var.location
-  size                  = "Standard_B1s"
-  admin_username        = "azureuser"
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  custom_data           = var.custom_data
-  disable_password_authentication = true
+  vnet_name             = var.vnet_name
+  vnet_address_space    = var.vnet_address_space
+  subnet_name           = var.subnet_name
+  subnet_address_prefixes = var.subnet_address_prefixes
+}
 
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = var.public_key
-  }
+module "nsg" {
+  source              = "./modules/nsg"
+  nsg_name            = var.nsg_name
+  location            = module.network.location
+  resource_group_name = module.network.resource_group_name
+}
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+module "vm" {
+  source              = "./modules/vm"
+  vm_name             = var.vm_name
+  location            = module.network.location
+  resource_group_name = module.network.resource_group_name
+  subnet_id           = module.network.subnet_id
+  nsg_id              = module.nsg.nsg_id
+  public_key          = var.public_key
+  custom_data         = local_file.cloud_init.content
 }
